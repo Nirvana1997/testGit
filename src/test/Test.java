e283a8b09309c55d2a9cdc1766ce7decd108ffb3
Nirvana1997 <408985936@qq.com>
2017-12-03 14:02:10 +0800

    测试git的冲突

---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        String test = "测试git的冲突功能";
+    }
+
+    public void testGit(){
+        String test = "测试冲突";
+    }
+}
