069e115196a3522ced7b3c833db4d1f265afcef9
Nirvana1997 <408985936@qq.com>
2017-12-03 14:08:11 +0800

    修改main

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,8 @@ package test;
 public class Test {
     public static void main(String[] args) {
         String test = "测试git的冲突功能";
+        String test1 = "测试git的冲突功能";
+        String test2 = "测试git的冲突功能";
     }
 
     public void testGit(){
